d68e7c006f5d7dcb0dbed83e117763484e25d486
Sergey <xxsmotur@gmail.com>
2020-02-10 13:13:13 +0500

    Reorganization of test storage

--- pkg/storage/redis/storage_test.go
+++ pkg/storage/redis/storage_test.go
@@ -11,6 +11,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type testFind struct {
+	Input   []models.Parking
+	Request *storage.FindConfig
+}
+
 func newTestRedis() storage.Storage {
 	mr, err := miniredis.Run()
 	if err != nil {
@@ -26,6 +31,15 @@ func newTestRedis() storage.Storage {
 		panic(err)
 	}
 	return client
+}
+
+func checkParkingStructFind(t *testing.T, r storage.Storage, req *storage.FindConfig) {
+	data, err := r.Find(req)
+	assert.NoError(t, err)
+	assert.Equal(t, len(data), 1)
+	assert.Equal(t, data[0].ID, int64(1))
+	assert.Equal(t, data[0].Mode, "test")
+	assert.Equal(t, data[0].GlobalID, int64(1))
 }
 
 func TestInsertParkingData(t *testing.T) {
@@ -58,19 +72,44 @@ func TestInsertParkingData(t *testing.T) {
 
 func TestFindParkingData(t *testing.T) {
 	r := newTestRedis()
-	count, err := r.Insert([]models.Parking{models.Parking{
-		ID:       1,
-		Mode:     "test",
-		GlobalID: 1,
-	}})
-	assert.NoError(t, err)
-	assert.Equal(t, count, 1)
-	data, err := r.Find(&storage.FindConfig{
-		ID: 1,
-	})
-	assert.NoError(t, err)
-	assert.Equal(t, len(data), 1)
-	assert.Equal(t, data[0].ID, int64(1))
-	assert.Equal(t, data[0].Mode, "test")
-	assert.Equal(t, data[0].GlobalID, int64(1))
+	inp := []testFind{testFind{
+		Input: []models.Parking{models.Parking{
+			ID:       1,
+			Mode:     "test",
+			GlobalID: 1,
+		}},
+		Request: &storage.FindConfig{
+			ID: 1,
+		},
+	},
+
+		testFind{
+			Input: []models.Parking{models.Parking{
+				ID:       1,
+				Mode:     "test",
+				GlobalID: 1,
+			}},
+			Request: &storage.FindConfig{
+				GlobalID: 1,
+			},
+		},
+
+		testFind{
+			Input: []models.Parking{models.Parking{
+				ID:       1,
+				Mode:     "test",
+				GlobalID: 1,
+			}},
+			Request: &storage.FindConfig{
+				ModeID: "test",
+			},
+		},
+	}
+
+	for _, d := range inp {
+		count, err := r.Insert(d.Input)
+		assert.NoError(t, err)
+		assert.Equal(t, count, len(d.Input))
+		checkParkingStructFind(t, r, d.Request)
+	}
 }
