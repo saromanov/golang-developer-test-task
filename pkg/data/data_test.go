6f790ed225e48982c666a6fc4eacc54c14129ca3
Sergey <xxsmotur@gmail.com>
2020-02-10 17:17:02 +0500

    Adding test for loading local data

--- pkg/data/data_test.go
+++ pkg/data/data_test.go
@@ -1,1 +1,13 @@
 package data
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadData(t *testing.T) {
+	data, err := LocalLoad("../assets/data.json")
+	assert.NoError(t, err)
+	assert.NotEqual(t, len(data), 0)
+}
